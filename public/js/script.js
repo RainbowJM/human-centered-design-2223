3f32748f97b0c8a2b9706a3b1d2390a0e3ec6b35
Jevona Magdalena <jmagdalena@ictual.com>
2023-04-20 10:42:48 +0200

    recording part in

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,36 @@
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recButton = document.getElementById('microphone');
+let recognition;
+let listen = false;
+let selectedText = "";
+let lastMessage = "";
 
 if (SpeechRecognition) {
-  const recognition = new SpeechRecognition();
-  recognition.lang = "en-US";
-  recognition.continuous = true;
-  recognition.interimResults = false;
+    recognition = new SpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.continuous = true;
+    recognition.interimResults = false;
+}
+
+recButton.addEventListener('change', function () {
+    if (this.checked) {
+        console.log("checked");
+        start()
+    }
+    else {
+        listen = false;
+        stop();
+    }
+});
+
+function start() {
+    listen = true;
+    alert("Mic is on");
+    recognition.start();
+}
+
+function stop() {
+    listen = false;
+    alert("Mic is off");
+    recognition.stop();
 }
